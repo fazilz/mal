26db31aca362c5d386021ac298ccec2897fb9686
Fazil Zuhaib <zuhaibfazil@gmail.com>
2024-04-07 23:22:23 -0400

    My cpp impl of mal.

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,42 +1,33 @@
-#include "String.h"
-#include "ReadLine.h"
-
+#include <string>
 #include <iostream>
-#include <memory>
 
-String READ(const String& input);
-String EVAL(const String& ast);
-String PRINT(const String& ast);
-String rep(const String& input);
-
-static ReadLine s_readLine("~/.mal-history");
-
-int main(int argc, char* argv[])
-{
-    String prompt = "user> ";
-    String input;
-    while (s_readLine.get(prompt, input)) {
-        std::cout << rep(input) << "\n";
-    }
-    return 0;
-}
-
-String rep(const String& input)
-{
-    return PRINT(EVAL(READ(input)));
-}
-
-String READ(const String& input)
-{
+std::string READ(std::string input) {
     return input;
 }
 
-String EVAL(const String& ast)
-{
-    return ast;
+std::string EVAL(std::string input) {
+    return input;
 }
 
-String PRINT(const String& ast)
-{
-    return ast;
+std::string PRINT(std::string input) {
+    return input;
+}
+
+std::string rep(std::string input) {
+    auto ast = READ(input);
+    auto result = EVAL(ast);
+    return PRINT(result);
+}
+
+int main() {
+
+    std::string input;
+    for (;;){
+        std::cout << "user> ";
+        if (!std::getline(std::cin, input)) {
+            break;
+        }
+        std::cout << rep(input) << std::endl;
+    }
+    return 0;
 }
